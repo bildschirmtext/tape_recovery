c66049a7ecf091aa66a0bfdbd233532da3cbd69d
casandro <christian@clarke-3.d>
2020-01-11 16:43:01 +0100

    Process.sh now works in /tmp directory and cleans up after itself.

--- src/process.sh
+++ src/process.sh
@@ -5,9 +5,12 @@ extension=.cpt
 for x in "$1"
 do
 	bn=`basename "$x" .wav | tr " " "_"`
+	tfile=`mktemp /tmp/XXXXXX`
+	tdir=$tfile-tmp
 	echo $x $bn
-	mkdir $bn
-	rm $bn/*
-	sox "$x" -r 6800 -c 1 -t dat -  | tail -n+3 | ./bin/demod | sox -t dat -r 3400 - -r 19200 -t dat - | tail -n+3 | ./bin/uart_deglitch | ./bin/uart_uart_split $bn/ | tee $bn/demod_info.txt
-	zip -r $bn $bn/
+	mkdir $tdir
+	sox "$x" -r 6800 -c 1 -t dat -  | tail -n+3 | ./bin/demod | sox -t dat -r 3400 - -r 19200 -t dat - | tail -n+3 | ./bin/uart_deglitch | ./bin/uart_uart_split $tdir/ | tee $tdir/demod_info.txt
+	zip -j $bn $tdir/*
+	rm -r $tdir
+	rm $tfile
 done
